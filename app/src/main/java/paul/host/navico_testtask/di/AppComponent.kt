3b061c01e00904de79264511601d5450432b7953
Paul Hostev <paul.hostev@gmail.com>
2019-06-17 18:49:27 +0300

    [api] - Network Module

--- app/src/main/java/paul/host/navico_testtask/di/AppComponent.kt
+++ app/src/main/java/paul/host/navico_testtask/di/AppComponent.kt
@@ -3,16 +3,23 @@ package paul.host.navico_testtask.di
 import dagger.Component
 import paul.host.navico_testtask.App
 import paul.host.navico_testtask.di.module.AppModule
+import paul.host.navico_testtask.di.module.NetworkModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(
+    modules = [
+        AppModule::class,
+        NetworkModule::class
+    ]
+)
 interface AppComponent {
 
     object Initializer {
         fun init(app: App): AppComponent {
             return DaggerAppComponent.builder()
                 .appModule(AppModule(app))
+                .networkModule(NetworkModule())
                 .build()
         }
     }
---
+++ app/src/main/java/paul/host/navico_testtask/di/module/NetworkModule.kt
@@ -0,0 +1,37 @@
+package paul.host.navico_testtask.di.module
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .cache(null)
+            .build()
+
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .baseUrl("https://api.github.com/")
+            .client(client)
+            .build()
+    }
+
+}
